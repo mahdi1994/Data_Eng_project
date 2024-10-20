3141b19e15e44d29694fde7f2b8209710872a08f
msallemmahdi <msalem.mehdi@gmail.com>
2024-10-20 19:26:37 +0200

    correct classes and main function for extracting

--- Extract_Data_CyclingRanking/main.py
+++ Extract_Data_CyclingRanking/main.py
@@ -1,30 +1,26 @@
 from Extract_Data_CyclingRanking.storage_acoount_sas_details import sas_token, storage_account_name
-from Scraper import RiderDataScraper
+from Scraper import CyclingDataScraper
 from DataLakeUploader import DataLakeUploader
 
-class RidersDataPipeline:
-    def __init__(self, scraper_url, blob_name):
-        account_url = f"https://{storage_account_name}.dfs.core.windows.net/?{sas_token}"
-        self.scraper = RiderDataScraper(scraper_url)
-        self.uploader = DataLakeUploader(account_url, "raw")
-        self.blob_name = blob_name
 
-    def run(self):
-        print("Scraping data...")
-        csv_data = self.scraper.extract_rider_data(limit=20)
 
-        print("Uploading data to Azure Data Lake...")
-        self.uploader.upload_data(self.blob_name, csv_data)
-
-        print("Pipeline completed.")
-
+def run(scraper, uploader, blob_name, with_headers):
+    print("Scraping data...")
+    csv_data = scraper.process_ranking_rows(with_headers, limit=20)
+    print("Uploading data to Azure Data Lake...")
+    uploader.upload_data(blob_name, csv_data)
+    print("Pipeline completed.")
 
 if __name__ == "__main__":
     # Define variables
-    for year in range(2009, 2025):
+    account_url = f"https://{storage_account_name}.dfs.core.windows.net/?{sas_token}"
+    table_class = "table table-striped"
+    folder = "raw/test"
+    for year in range(2009, 2010):
         URL = f"https://www.cyclingranking.com/riders/{year}"
         BLOB_NAME = f"riders_data_streamed{year}.csv"
-
+        scrapper = CyclingDataScraper(URL, table_class)
+        uploader = DataLakeUploader(account_url, folder)
     # Run the data pipeline
-        pipeline = RidersDataPipeline(URL, BLOB_NAME)
-        pipeline.run()
+        #pipeline = RidersDataPipeline(URL, BLOB_NAME)
+        run(scrapper, uploader, BLOB_NAME, False)
